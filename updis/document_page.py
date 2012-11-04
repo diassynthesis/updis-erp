9a21e3a253d61f6f67af93765d594a1f78d4cfc5
Shrek zhou <shrek@ubuntu.(none)>
2012-11-04 23:13:30 +0800

    changes

--- updis/document_page.py
+++ updis/document_page.py
@@ -3,8 +3,8 @@ from osv import osv,fields
 class document_page(osv.osv):
 	_inherit="document.page"
 	_columns = {
-		'content': fields.html("Content"),
-		'display_name':fields.boolean("Dsplay Name?",help="Check this if you don't want to display your name"),
+		# 'content': fields.html("Content"),
+		'display_name':fields.boolean("Display Name?",help="Check this if you don't want to display your name"),
 		'read_times': fields.integer("Read Times"),
 		'department_id':fields.many2one("hr.department","Department"),
 		'display_position':fields.selection([('top','Top'),('content','Content')],"Display Position"),
