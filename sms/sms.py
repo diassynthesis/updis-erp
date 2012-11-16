897e21fe153642020e2e10858e70dd4f91ee520f
Zhou Guangwen <zgwmike@gmail.com>
2012-11-17 01:41:11 +0800

    'changes'

--- sms/sms.py
+++ sms/sms.py
@@ -6,5 +6,7 @@ class sms(osv.Model):
 		'content':fields.text("Content",size=128),
 		'from':fields.char("From",size=64),
 		'to':fields.char('To',size=64),
-		'sent_date':fields.datetime("Send date"),		
+		'sent_date':fields.datetime("Send date"),	
+		'model':fields.char("Related Document Model",size=128,select=1),
+		'res_id':fields.integer('Related Document ID',select=1),		
 	}
