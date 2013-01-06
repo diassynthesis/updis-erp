28f7d63a8c52f64362d9bca2dd7185e66bb45663
Shrek Zhou <zgwmike@hotmail.com>
2013-01-06 00:34:41 -0500

    changes

--- updis/project.py
+++ updis/project.py
@@ -41,57 +41,57 @@ class updis_project(osv.osv):
 		# 基础信息
 		"state_id":fields.many2one('res.country.state','State'),
 		"city":fields.char("City",size=128),
-		"guimo":fields.char(u"规模",size=64),
-		"waibao":fields.boolean(u"是否外包"),
-		"shizhenpeitao":fields.boolean(u"市政配套"),
-		"duofanghetong":fields.boolean(u"多方合同"),
-		"jianyishejibumen_id":fields.many2one("hr.department",u"建议设计部门"),
-		"jianyixiangmufuzeren_id":fields.many2one("res.users",u"建议项目负责人"),
-		"shifoutoubiao":fields.boolean(u"是否投标项目"),
-		"jiafang_id":fields.many2one('res.partner', u"甲方"),
-		'gongzuodagang':fields.boolean(u'有工作大纲（保存归档）'),
-		'chuangyouxiangmu':fields.boolean(u'创优项目'),
-		'zhuantihuozixiang':fields.boolean(u'专题或子项（详见工作大纲）'),
-		'youfenbaofang':fields.boolean(u'有分包方（详见分包协议）'),
+		"guimo":fields.char(u"规模",size=64,readonly=True),
+		"waibao":fields.boolean(u"是否外包",readonly=True),
+		"shizhenpeitao":fields.boolean(u"市政配套",readonly=True),
+		"duofanghetong":fields.boolean(u"多方合同",readonly=True),
+		"jianyishejibumen_id":fields.many2one("hr.department",u"建议设计部门",readonly=True),
+		"jianyixiangmufuzeren_id":fields.many2one("res.users",u"建议项目负责人",readonly=True),
+		"shifoutoubiao":fields.boolean(u"是否投标项目",readonly=True),
+		"jiafang_id":fields.many2one('res.partner', u"甲方",readonly=True),
+		'gongzuodagang':fields.boolean(u'有工作大纲（保存归档）',readonly=True),
+		'chuangyouxiangmu':fields.boolean(u'创优项目',readonly=True),
+		'zhuantihuozixiang':fields.boolean(u'专题或子项（详见工作大纲）',readonly=True),
+		'youfenbaofang':fields.boolean(u'有分包方（详见分包协议）',readonly=True),
 		
 		# 所长审批
 		"yaoqiuxingchengwenjian":fields.selection([
 			(u"已形成",u"已形成"),
 			(u"未形成，但已确认",u"未形成，但已确认")],
-			u"顾客要求形成文件否"),		
-		"zhaobiaoshu":fields.boolean(u"有招标书"),# 明示要求
-		"weituoshu":fields.boolean(u"有委托书"),# 明示要求
-		"xieyicaoan":fields.boolean(u"有协议/合同草案"),# 明示要求
-		"koutouyaoqiujilu":fields.boolean(u"有口头要求记录"),# 明示要求
-		"yinhanyaoqiu":fields.selection([(u"有",u"有（需在评审记录一栏中标明记录）"),(u"无",u"无")],u"隐含要求"),
-		"difangfagui":fields.selection([(u"有",u"有（需在评审记录一栏中标明记录）"),(u"无",u"无")],u"地方规范或特殊法律法规"),
-		"fujiayaoqiu":fields.selection([(u"有",u"有（需在评审记录一栏中标明记录）"),(u"无",u"无")],u"附加要求"),
+			u"顾客要求形成文件否",readonly=True),		
+		"zhaobiaoshu":fields.boolean(u"有招标书",readonly=True),# 明示要求
+		"weituoshu":fields.boolean(u"有委托书",readonly=True),# 明示要求
+		"xieyicaoan":fields.boolean(u"有协议/合同草案",readonly=True),# 明示要求
+		"koutouyaoqiujilu":fields.boolean(u"有口头要求记录",readonly=True),# 明示要求
+		"yinhanyaoqiu":fields.selection([(u"有",u"有（需在评审记录一栏中标明记录）"),(u"无",u"无")],u"隐含要求",readonly=True),
+		"difangfagui":fields.selection([(u"有",u"有（需在评审记录一栏中标明记录）"),(u"无",u"无")],u"地方规范或特殊法律法规",readonly=True),
+		"fujiayaoqiu":fields.selection([(u"有",u"有（需在评审记录一栏中标明记录）"),(u"无",u"无")],u"附加要求",readonly=True),
 		"hetongyizhi":fields.selection([(u"合同/协议要求表述不一致已解决",u"合同/协议要求表述不一致已解决"),
-			(u"没有出现不一致",u"没有出现不一致")],u"不一致是否解决"),	
-		"ziyuan":fields.selection([(u'人力资源满足',u'人力资源满足'),(u'人力资源不足',u'人力资源不足')],u'人力资源'),#本院是否有能力满足规定要求
-		"shebei":fields.selection([(u'设备满足','设备满足'),(u'设备不满足',u'设备不满足')],u"设备"),#本院是否有能力满足规定要求
-		"gongqi":fields.selection([(u'工期可接受','工期可接受'),(u'工期太紧',u'工期太紧')],u"工期"),#本院是否有能力满足规定要求
-		"shejifei":fields.selection([(u'设计费合理','设计费合理'),(u'设计费太低',u'设计费太低')],u'设计费'),#本院是否有能力满足规定要求
+			(u"没有出现不一致",u"没有出现不一致")],u"不一致是否解决",readonly=True),	
+		"ziyuan":fields.selection([(u'人力资源满足',u'人力资源满足'),(u'人力资源不足',u'人力资源不足')],u'人力资源',readonly=True),#本院是否有能力满足规定要求
+		"shebei":fields.selection([(u'设备满足','设备满足'),(u'设备不满足',u'设备不满足')],u"设备",readonly=True),#本院是否有能力满足规定要求
+		"gongqi":fields.selection([(u'工期可接受','工期可接受'),(u'工期太紧',u'工期太紧')],u"工期",readonly=True),#本院是否有能力满足规定要求
+		"shejifei":fields.selection([(u'设计费合理','设计费合理'),(u'设计费太低',u'设计费太低')],u'设计费',readonly=True),#本院是否有能力满足规定要求
 
 		# 经营室
-		"xiangmubianhao":fields.char(u"项目编号",select=True,size=128),
-		"pingshenfangshi":fields.selection([(u'会议',u'会议'),(u'会签',u'会签'),(u'审批',u'审批')],u"评审方式"),
+		"xiangmubianhao":fields.char(u"项目编号",select=True,size=128,readonly=True),
+		"pingshenfangshi":fields.selection([(u'会议',u'会议'),(u'会签',u'会签'),(u'审批',u'审批')],u"评审方式",readonly=True),
 		"yinfacuoshi":fields.selection([(u'可以接受',u'可以接受'),(u'不接受',u'不接受'),(u'加班',u'加班'),
-			(u'院内调配',u'院内调配'),(u'外协',u'外协'),(u'其它',u'其它')],u"引发措施记录"),
+			(u'院内调配',u'院内调配'),(u'外协',u'外协'),(u'其它',u'其它')],u"引发措施记录",readonly=True),
 		"renwuyaoqiu":fields.selection([(u'见委托书',u'见委托书'),(u'见合同草案',u'见合同草案'),(u'见洽谈记录',u'见洽谈记录'),
-			(u'见电话记录',u'见电话记录'),(u'招标文件',u'招标文件')],u"任务要求"),
-		"chenjiebumen_id":fields.many2one("hr.department",u"承接部门"),
+			(u'见电话记录',u'见电话记录'),(u'招标文件',u'招标文件')],u"任务要求",readonly=True),
+		"chenjiebumen_id":fields.many2one("hr.department",u"承接部门",readonly=True),
 
 		# 总师室
-		"categories_id":fields.many2many("project.upcategory","up_project_category_rel","project_id","category_id",u"项目类别"),
-		"toubiaoleibie":fields.selection([(u'商务标',u'商务标'),(u'技术标',u'技术标'),(u'综合标',u'综合标')],u"投标类别"),
+		"categories_id":fields.many2many("project.upcategory","up_project_category_rel","project_id","category_id",u"项目类别",readonly=True),
+		"toubiaoleibie":fields.selection([(u'商务标',u'商务标'),(u'技术标',u'技术标'),(u'综合标',u'综合标')],u"投标类别",readonly=True),
 		"guanlijibie":fields.selection([
 			(u'院级',u'院级'),
-			(u'所级',u'所级')],u'项目管理级别'),
-		"chenjiefuzeren_id":fields.many2one("res.users",u"承接项目负责人"),
-		"zhuguanzongshi_id":fields.many2one("res.users",u"主管总师"),
+			(u'所级',u'所级')],u'项目管理级别',readonly=True),
+		"chenjiefuzeren_id":fields.many2one("res.users",u"承接项目负责人",readonly=True),
+		"zhuguanzongshi_id":fields.many2one("res.users",u"主管总师",readonly=True),
 		
-		'assignment_ids':fields.one2many('project.assignment','project_id','Project Assignment'),
+		'assignment_ids':fields.one2many('project.assignment','project_id','Project Assignment',readonly=True),
 		"state":fields.selection([
 			# ("draft",u"New project"),
 			("open",u"任意人员填写申请单"),
