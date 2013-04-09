3c30460303b1e69aa9786fa56d5e548f5fcc1e3c
cysnake4713 <cysnake4713@gmail.com>
2013-04-09 17:38:13 +0800

    UPDIS-68 seems fix

--- updis/static/src/js/updis.js
+++ updis/static/src/js/updis.js
@@ -21,32 +21,77 @@ openerp.updis = function(openerp) {
 				return msg;
 			}
 		})
-		openerp.mail.ThreadComposeMessage.include({		
-			do_send_message_post: function (partner_ids) {
-				var self = this;
-				this.parent_thread.ds_thread._model.call('message_post_user_api', [this.context.default_res_id], {
-					'body': this.$('textarea').val(),
-					'subject': false,
-					'parent_id': this.context.default_parent_id,
-					'attachment_ids': _.map(this.attachment_ids, function (file) {return file.id;}),
-					'partner_ids': partner_ids,
-					'context': this.parent_thread.context,
-					'is_anonymous': this.$('.oe_is_anonymous').is(":checked"),
-				}).done(function (message_id) {
-					var thread = self.parent_thread;
-					var root = thread == self.options.root_thread;
-					if (self.options.display_indented_thread < self.thread_level && thread.parent_message) {
-						var thread = thread.parent_message.parent_thread;
-					}
-					// create object and attach to the thread object
-					thread.message_fetch([["id", "=", message_id]], false, [message_id], function (arg, data) {
-						var message = thread.create_message_object( data[0] );
-						// insert the message on dom
-						thread.insert_message( message, root ? undefined : self.$el, root );
-					});
-					self.on_cancel();
-				});
-			}
-		})
+//		openerp.mail.ThreadComposeMessage.include({
+//			do_send_message_post: function (partner_ids) {
+//				var self = this;
+//				this.parent_thread.ds_thread._model.call('message_post_user_api', [this.context.default_res_id], {
+//					'body': this.$('textarea').val(),
+//					'subject': false,
+//					'parent_id': this.context.default_parent_id,
+//					'attachment_ids': _.map(this.attachment_ids, function (file) {return file.id;}),
+//					'partner_ids': partner_ids,
+//					'context': this.parent_thread.context,
+//					'is_anonymous': this.$('.oe_is_anonymous').is(":checked"),
+//				}).done(function (message_id) {
+//					var thread = self.parent_thread;
+//					var root = thread == self.options.root_thread;
+//					if (self.options.display_indented_thread < self.thread_level && thread.parent_message) {
+//						var thread = thread.parent_message.parent_thread;
+//					}
+//					// create object and attach to the thread object
+//					thread.message_fetch([["id", "=", message_id]], false, [message_id], function (arg, data) {
+//						var message = thread.create_message_object( data[0] );
+//						// insert the message on dom
+//						thread.insert_message( message, root ? undefined : self.$el, root );
+//					});
+//					self.on_cancel();
+//				});
+//			}
+//		})
+
+        /* do post a message and fetch the message */
+
+        openerp.mail.ThreadComposeMessage.include({
+
+            do_send_message_post: function (partner_ids, log) {
+                var self = this;
+                var values = {
+                    'body': this.$('textarea').val(),
+                    'subject': false,
+                    'parent_id': this.context.default_parent_id,
+                    'attachment_ids': _.map(this.attachment_ids, function (file) {return file.id;}),
+                    'partner_ids': partner_ids,
+                    'context': _.extend(this.parent_thread.context, {
+                        'mail_post_autofollow': true,
+                        'mail_post_autofollow_partner_ids': partner_ids,
+                    }),
+                    'type': 'comment',
+                    'content_subtype': 'plaintext',
+                    'body': this.$('textarea').val(),
+                    'is_anonymous': this.$('.oe_is_anonymous').is(":checked")
+                };
+                if (log) {
+                    values['subtype'] = false;
+                }
+                else {
+                    values['subtype'] = 'mail.mt_comment';
+                }
+                this.parent_thread.ds_thread._model.call('message_post', [this.context.default_res_id], values).done(function (message_id) {
+                    var thread = self.parent_thread;
+                    var root = thread == self.options.root_thread;
+                    if (self.options.display_indented_thread < self.thread_level && thread.parent_message) {
+                        var thread = thread.parent_message.parent_thread;
+                    }
+                    // create object and attach to the thread object
+                    thread.message_fetch([["id", "=", message_id]], false, [message_id], function (arg, data) {
+                        var message = thread.create_message_object( data[0] );
+                        // insert the message on dom
+                        thread.insert_message( message, root ? undefined : self.$el, root );
+                    });
+                    self.on_cancel();
+                });
+            }
+        })
+
 	};
 }
