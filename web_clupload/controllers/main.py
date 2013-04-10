33e568d67538849729ce137d5c2639145cbe2e64
cysnake4713 <cysnake4713@gmail.com>
2013-04-10 19:19:36 +0800

    UPDIS-59 not done yet

--- web_clupload/controllers/main.py
+++ web_clupload/controllers/main.py
@@ -6,46 +6,74 @@ import tools
 import os
 import openerp
 import time
+import ftputil
 
-from openerp.addons.web.controllers.main import manifest_list, module_boot,html_template
+from openerp.addons.web.controllers.main import manifest_list, module_boot, html_template
+
 
 class InternalHome(openerp.addons.web.http.Controller):
-	_cp_path = "/web/clupload"
-	def _create_attachment(self,req,qqfile,resize_image=False):
-		context = req.context
-		Model = req.session.model('ir.attachment')
+    _cp_path = "/web/clupload"
 
-		datas = base64.encodestring(req.httprequest.data)
-		if resize_image:
-			datas = tools.image_resize_image(datas,size=(700,700))
-		attachment_id = Model.create({
-			'name': qqfile,
-			'datas': datas,
-			'datas_fname': qqfile,
-		}, context)		
-		args = {
-			'filename': qqfile,
-			'id':  attachment_id,
-			'success': True,			
-		}
-		return args
-	@openerp.addons.web.http.httprequest
-	def upload_image(self, req, qqfile):		
-		try:	
-			args = self._create_attachment(req,qqfile,True)		
-			# model, id, field, **kw
-			url = '%sweb/binary/image?session_id=%s&model=ir.attachment&field=datas&id=%s'%(req.httprequest.url_root, req.session_id, args['id'])
-			args['url'] = url
-		except Exception, e:
-			args = { 'error': e.message}
-		return req.make_response(simplejson.dumps(args))
-	@openerp.addons.web.http.httprequest
-	def upload_file(self, req, qqfile):
-		try:	
-			args = self._create_attachment(req,qqfile)		
-			# model, field, id=None, filename_field=None, **kw)
-			url = '%sweb/binary/saveas?session_id=%s&model=ir.attachment&field=datas&filename_field=datas_fname&id=%s'%(req.httprequest.url_root, req.session_id, args['id'])
-			args['url'] = url
-		except Exception, e:
-			args = { 'error': e.message}
-		return req.make_response(simplejson.dumps(args))
+
+    def _upload_file_ftp(self, file_data, file_name):
+        HOME = os.getenv("HOME") + '/tempfile'
+        if not os.path.exists(HOME):
+            os.mkdir(HOME)
+
+        output = open(HOME + '/' + file_name, 'wb')
+        output.write(file_data)
+        output.close()
+        try:
+            host = ftputil.FTPHost('10.100.100.196', 'ftpuser', 'updis_ftp_2013')
+            host.upload(HOME + '/' + file_name,'/'+file_name)
+        except Exception, e:
+            args = {'error': e.message}
+        finally:
+            host.close()
+            return args
+
+    def _create_attachment(self, req, qqfile, resize_image=False):
+        context = req.context
+        Model = req.session.model('ir.attachment')
+
+        # datas = base64.encodestring(req.httprequest.data)
+        # if resize_image:
+        #     datas = tools.image_resize_image(datas, size=(700, 700))
+
+        args = self._upload_file_ftp(req.httprequest.data, qqfile)
+
+        # attachment_id = Model.create({
+        #                                  'name': qqfile,
+        #                                  'datas': datas,
+        #                                  'datas_fname': qqfile,
+        #                              }, context)
+
+        args = {
+            'filename': qqfile,
+            'success': True,
+        }
+        return args
+
+    @openerp.addons.web.http.httprequest
+    def upload_image(self, req, qqfile):
+        try:
+            args = self._create_attachment(req, qqfile, True)
+            # model, id, field, **kw
+            url = '%sweb/binary/image?session_id=%s&model=ir.attachment&field=datas&id=%s' % (
+                req.httprequest.url_root, req.session_id, args['id'])
+            args['url'] = url
+        except Exception, e:
+            args = {'error': e.message}
+        return req.make_response(simplejson.dumps(args))
+
+    @openerp.addons.web.http.httprequest
+    def upload_file(self, req, qqfile):
+        try:
+            args = self._create_attachment(req, qqfile)
+            # model, field, id=None, filename_field=None, **kw)
+            url = '%sweb/binary/saveas?session_id=%s&model=ir.attachment&field=datas&filename_field=datas_fname&id=%s' % (
+                req.httprequest.url_root, req.session_id, args['id'])
+            args['url'] = url
+        except Exception, e:
+            args = {'error': e.message}
+        return req.make_response(simplejson.dumps(args))
