ab1deaacfaad91a768ecf55f27481fcec1dd7200
cysnake4713 <cysnake4713@gmail.com>
2013-04-11 17:16:20 +0800

    UPDIS-59 done,need update ftp url

--- web_clupload/controllers/main.py
+++ web_clupload/controllers/main.py
@@ -1,35 +1,54 @@
 # -*- coding: utf-8 -*-
-import logging
+import hashlib
+import random
 import simplejson
-import base64
-import tools
 import os
-import openerp
 import time
-import ftputil
 
-from openerp.addons.web.controllers.main import manifest_list, module_boot, html_template
+import openerp
+import ftputil
+from openerp.addons.web.controllers.main import manifest_list
+
 
 
 class InternalHome(openerp.addons.web.http.Controller):
     _cp_path = "/web/clupload"
 
 
+
+    def _generate_file_name(self, s_file_name):
+
+        def _get_salt(current_time, filename):
+            salt = str(random.random)
+            h = hashlib.md5(current_time + salt).hexdigest()
+            return h
+
+        current_time = time.strftime('%Y-%m-%d_%H-%M', time.localtime(time.time()))
+        salt = _get_salt(current_time, s_file_name)
+        return '%s_%s%s' % (current_time, salt, os.path.splitext(s_file_name)[1])
+
+
     def _upload_file_ftp(self, file_data, file_name):
         HOME = os.getenv("HOME") + '/tempfile'
         if not os.path.exists(HOME):
             os.mkdir(HOME)
-
-        output = open(HOME + '/' + file_name, 'wb')
+        new_file_name = self._generate_file_name(file_name)
+        output = open(HOME + '/' + new_file_name, 'wb')
         output.write(file_data)
         output.close()
         try:
             host = ftputil.FTPHost('10.100.100.196', 'ftpuser', 'updis_ftp_2013')
-            host.upload(HOME + '/' + file_name,'/'+file_name)
+            host.upload(HOME + '/' + str(new_file_name), '/' + str(new_file_name))
+            args = {
+                'filename': new_file_name,
+                'success': True,
+            }
         except Exception, e:
             args = {'error': e.message}
         finally:
             host.close()
+            if os.path.isfile(HOME + '/' + new_file_name):
+                os.remove(HOME + '/' + new_file_name)
             return args
 
     def _create_attachment(self, req, qqfile, resize_image=False):
@@ -47,11 +66,6 @@ class InternalHome(openerp.addons.web.http.Controller):
         #                                  'datas': datas,
         #                                  'datas_fname': qqfile,
         #                              }, context)
-
-        args = {
-            'filename': qqfile,
-            'success': True,
-        }
         return args
 
     @openerp.addons.web.http.httprequest
@@ -59,8 +73,7 @@ class InternalHome(openerp.addons.web.http.Controller):
         try:
             args = self._create_attachment(req, qqfile, True)
             # model, id, field, **kw
-            url = '%sweb/binary/image?session_id=%s&model=ir.attachment&field=datas&id=%s' % (
-                req.httprequest.url_root, req.session_id, args['id'])
+            url = 'http://10.100.100.196:8000/%s' % (args['filename'])
             args['url'] = url
         except Exception, e:
             args = {'error': e.message}
@@ -71,8 +84,7 @@ class InternalHome(openerp.addons.web.http.Controller):
         try:
             args = self._create_attachment(req, qqfile)
             # model, field, id=None, filename_field=None, **kw)
-            url = '%sweb/binary/saveas?session_id=%s&model=ir.attachment&field=datas&filename_field=datas_fname&id=%s' % (
-                req.httprequest.url_root, req.session_id, args['id'])
+            url = 'http://10.100.100.196:8000/%s' % (args['filename'])
             args['url'] = url
         except Exception, e:
             args = {'error': e.message}
