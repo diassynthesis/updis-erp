7ce9fe6356edd181ddd153993bfbb8886c647042
cysnake4713 <cysnake4713@gmail.com>
2013-04-22 17:13:36 +0800

    UPDIS-59 - ERP和CMS中文件上传功能 done

--- web_clupload/controllers/main.py
+++ web_clupload/controllers/main.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import ftplib
 import hashlib
 import random
 import simplejson
@@ -10,11 +11,20 @@ import ftputil
 from openerp.addons.web.controllers.main import manifest_list
 
 
+class MySession(ftplib.FTP):
+    def __init__(self, host, userid, password, port):
+        """Act like ftplib.FTP's constructor but connect to another port."""
+        ftplib.FTP.__init__(self)
+        self.connect(host, port)
+        self.login(userid, password)
+
 
 class InternalHome(openerp.addons.web.http.Controller):
     _cp_path = "/web/clupload"
-
-
+    _FTP_ADDRESS = 'ftp.updis.cn'
+    _FTP_PORT = 2121
+    _FTP_USER_NAME = 'ftpuser'
+    _FTP_PASSWORD = 'updis_ftp_2013'
 
     def _generate_file_name(self, s_file_name):
 
@@ -36,17 +46,20 @@ class InternalHome(openerp.addons.web.http.Controller):
         output = open(HOME + '/' + new_file_name, 'wb')
         output.write(file_data)
         output.close()
+        host = None
         try:
-            host = ftputil.FTPHost('10.100.100.196', 'ftpuser', 'updis_ftp_2013')
-            host.upload(HOME + '/' + str(new_file_name), '/' + str(new_file_name))
+            host = ftputil.FTPHost(self._FTP_ADDRESS, self._FTP_USER_NAME, self._FTP_PASSWORD, port=self._FTP_PORT,
+                                   session_factory=MySession)
+            host.upload(HOME + '/' + str(new_file_name), '/erpupload/' + str(new_file_name))
             args = {
                 'filename': new_file_name,
                 'success': True,
             }
         except Exception, e:
-            args = {'error': e.message}
+            args = {'error': e.message, 'filename': file_name}
         finally:
-            host.close()
+            if host:
+                host.close()
             if os.path.isfile(HOME + '/' + new_file_name):
                 os.remove(HOME + '/' + new_file_name)
             return args
@@ -73,10 +86,10 @@ class InternalHome(openerp.addons.web.http.Controller):
         try:
             args = self._create_attachment(req, qqfile, True)
             # model, id, field, **kw
-            url = 'http://10.100.100.196:8000/%s' % (args['filename'])
+            url = 'http://ftp.updis.cn:8000/erpupload/%s' % (args['filename'])
             args['url'] = url
         except Exception, e:
-            args = {'error': e.message}
+            args = {'error': e.message, 'filename': qqfile}
         return req.make_response(simplejson.dumps(args))
 
     @openerp.addons.web.http.httprequest
@@ -84,8 +97,8 @@ class InternalHome(openerp.addons.web.http.Controller):
         try:
             args = self._create_attachment(req, qqfile)
             # model, field, id=None, filename_field=None, **kw)
-            url = 'http://10.100.100.196:8000/%s' % (args['filename'])
+            url = 'http://ftp.updis.cn:8000/erpupload/%s' % (args['filename'])
             args['url'] = url
         except Exception, e:
-            args = {'error': e.message}
+            args = {'error': e.message, 'filename': qqfile}
         return req.make_response(simplejson.dumps(args))
