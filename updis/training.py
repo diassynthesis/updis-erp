89a4fb7ea5e402be0e26dc83b5431eb1d01f53e4
cysnake4713 <cysnake4713@gmail.com>
2013-05-20 19:08:28 +0800

    UPDIS-187 人员培训管理模块制作

--- updis/training.py
+++ updis/training.py
@@ -9,7 +9,7 @@ class updis_hr_training(osv.osv):
     _columns = {
         'name': fields.char(size=100, string='Training Name', required=True),
         'time': fields.date(string='Training Time'),
-        'organizers': fields.char(size=100, string='Training Organizers'), #TODO: what's this one?
+        'organizers': fields.char(size=100, string='Training Organizers'),
         'record_ids': fields.one2many('updis.hr.training.record', 'training_id', 'Training Related employees'),
 
     }
@@ -22,11 +22,15 @@ class updis_hr_training_record(osv.osv):
         'training_id': fields.many2one('updis.hr.training', 'Training Name', required=True),
         'employee': fields.many2one('hr.employee', 'Employee', required=True),
 
-        'score': fields.char(size=100, string='Score'), #TODO: what's this one?
+        'score': fields.selection([(u'pass', u'Pass'), (u'fail', u'Fail')], 'Score'),
         'training_certificate': fields.char(string='Certificate Number', size=100),
         'training_time': fields.related('training_id', 'time', type="date",
                                         string="Training Time", readonly=True),
         'training_organizers': fields.related('training_id', 'organizers', type="char",
                                               string="Training Organizers", readonly=True),
+    }
+
+    _defaults = {
+        'score': u'pass',
     }
 
