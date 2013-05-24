746d97f9889da178d48dac455c08aeb568e0b281
cysnake4713 <cysnake4713@gmail.com>
2013-05-24 16:27:34 +0800

    UPDIS-177 正式发布前最后的细节修改

--- message/message.py
+++ message/message.py
@@ -273,7 +273,7 @@ class Message(osv.Model):
             sms = self.pool.get('sms.sms')
             message = self.pool.get('message.message').browse(cr, uid, mid, context=context)
             if message.is_allow_send_sms:
-                to = ','.join([rid.mobile_phone for rid in message.sms_receiver_ids if rid.mobile_phone])
+                to = ','.join([rid.mobile_phone.strip() for rid in message.sms_receiver_ids if rid.mobile_phone.strip()])
                 if to:
                     content = message.sms and message.category_id.name + ':' + message.sms or message.category_id.name + ':' + message.name
                     sid = sms.create(cr, uid, {'to': to, 'content': content, 'model': 'message.message', 'res_id': mid},
