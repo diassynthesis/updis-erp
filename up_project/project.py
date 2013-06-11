eebdbbabc75e1ef6310c8ba849fd50e60f21dc20
cysnake4713 <cysnake4713@gmail.com>
2013-06-12 03:19:25 +0800

    UPDIS-231 - 项目管理模块的归正

--- up_project/project.py
+++ up_project/project.py
@@ -54,25 +54,6 @@ class updis_project(osv.osv):
         "xiangmubianhao": fields.char(u"项目编号", select=True, size=128, ),
 
 
-        "yaoqiuxingchengwenjian": fields.selection([
-                                                       (u"已形成", u"已形成"),
-                                                       (u"未形成，但已确认", u"未形成，但已确认")],
-                                                   u"顾客要求形成文件否"),
-        # 所长审批
-        "express_requirement": fields.selection([(u"有招标书", u"有招标书"), (u"有委托书", u"有委托书"),
-                                                 (u"有协议/合同草案", u"有协议/合同草案"), (u"有口头要求记录", u"有口头要求记录")],
-                                                string="Express Requirement"),
-        "yinhanyaoqiu": fields.selection([(u"有", u"有（需在评审记录一栏中标明记录）"), (u"无", u"无")], u"隐含要求"),
-        "difangfagui": fields.selection([(u"有", u"有（需在评审记录一栏中标明记录）"), (u"无", u"无")], u"地方规范或特殊法律法规", ),
-        "fujiayaoqiu": fields.selection([(u"有", u"有（需在评审记录一栏中标明记录）"), (u"无", u"无")], u"附加要求", ),
-        "hetongyizhi": fields.selection([(u"合同/协议要求表述不一致已解决", u"合同/协议要求表述不一致已解决"),
-                                         (u"没有出现不一致", u"没有出现不一致")], u"不一致是否解决", ),
-        "ziyuan": fields.selection([(u'人力资源满足', u'人力资源满足'), (u'人力资源不足', u'人力资源不足')], u'人力资源', ),
-        #本院是否有能力满足规定要求
-        "shebei": fields.selection([(u'设备满足', '设备满足'), (u'设备不满足', u'设备不满足')], u"设备", ), #本院是否有能力满足规定要求
-        "gongqi": fields.selection([(u'工期可接受', '工期可接受'), (u'工期太紧', u'工期太紧')], u"工期", ), #本院是否有能力满足规定要求
-        "shejifei": fields.selection([(u'设计费合理', '设计费合理'), (u'设计费太低', u'设计费太低')], u'设计费', ), #本院是否有能力满足规定要求
-
 
 
 
