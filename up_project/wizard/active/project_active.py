ba7d7db4cdd456cc3985199a4ec5f4af9fb9dcdc
cysnake4713 <cysnake4713@gmail.com>
2013-07-18 12:47:10 +0800

    UPDIS-231		项目管理模块的归正 temp

--- up_project/wizard/active/project_active.py
+++ up_project/wizard/active/project_active.py
@@ -19,3 +19,8 @@ class project_project_inherit(osv.osv):
 
     def act_active_workflow(self, cr, uid, ids, context=None):
         return ids[0]
+
+    def action_end_active(self, cr, uid, ids, context=None):
+        self.pool.get('project.project.active.tasking')
+        self._workflow_signal(cr, uid, ids, 'temp_start_to_end', context=context)
+        return True
--- up_project1/__openerp__.py
+++ up_project1/__openerp__.py
@@ -42,6 +42,6 @@ UPDIS Project Module.""",
     ],
     'demo_xml': [],
     'test': [],
-    'installable': True,
+    'installable': False,
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
