e84fba8934492571c7c58f730207a2e87b3809c8
cysnake4713 <cysnake4713@gmail.com>
2013-07-31 14:23:47 +0800

    UPDIS-265 培训部分加上从什么时间到什么时间

--- updis/training.py
+++ updis/training.py
@@ -6,11 +6,12 @@ from osv import fields, osv
 class updis_hr_training(osv.osv):
     _description = "Employee Training"
     _name = "updis.hr.training"
+    _order = "time"
 
     _columns = {
         'name': fields.char(size=200, string='Training Name', required=True),
         'time': fields.date(string='Training Time'),
-        'end_time':fields.date(string='Training End Time'),
+        'end_time': fields.date(string='Training End Time'),
         'organizers': fields.char(size=100, string='Training Organizers'),
         'record_ids': fields.one2many('updis.hr.training.record', 'training_id', 'Training Related employees'),
 
@@ -20,6 +21,7 @@ class updis_hr_training(osv.osv):
 class updis_hr_training_record(osv.osv):
     _description = "Employee Training Record"
     _name = "updis.hr.training.record"
+    _order = "training_time"
     _columns = {
         'training_id': fields.many2one('updis.hr.training', 'Training Name', required=True),
         'employee': fields.many2one('hr.employee', 'Employee', required=True),
@@ -27,9 +29,9 @@ class updis_hr_training_record(osv.osv):
         'score': fields.selection([(u'pass', u'Pass'), (u'fail', u'Not Pass')], 'Score'),
         'training_certificate': fields.char(string='Certificate Number', size=100),
         'training_time': fields.related('training_id', 'time', type="date",
-                                        string="Training Time", readonly=True),
+                                        string="Training Time", readonly=True, store=True),
         'training_end_time': fields.related('training_id', 'end_time', type="date",
-                                        string="Training End Time", readonly=True),
+                                            string="Training End Time", readonly=True),
         'training_organizers': fields.related('training_id', 'organizers', type="char",
                                               string="Training Organizers", readonly=True),
     }
