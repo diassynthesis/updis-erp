15b62e4b4faceef8459896188d84bd848369b67c
cysnake4713 <cysnake4713@gmail.com>
2013-08-19 15:42:54 +0800

    UPDIS-300 消息发布能否与大蚂蚁关联？ 基本完成 ，目前无法传url

--- sms/sms.py
+++ sms/sms.py
@@ -8,9 +8,11 @@ from osv import osv, fields
 
 class sms(osv.Model):
     _sms_gateway = "http://web.mobset.com/SDK/Sms_Send.asp"
+    _big_ant_gateway = "http://10.100.100.200:6664/Employee.asmx/SendMessenge"
     _name = "sms.sms"
     _order = "sent_date desc"
     _columns = {
+        'subject': fields.char('Subject', size=256),
         'content': fields.text("Content", size=128),
         'from': fields.char("From", size=200),
         'to': fields.char('To', size=2000),
@@ -20,10 +22,70 @@ class sms(osv.Model):
         'sms_server_id': fields.char("SMS ID From Gateway", size=2000),
         'state': fields.selection([('draft', 'Draft'), ('error', 'Error'), ('sent', 'Sent')], 'State', required=True,
                                   size=64),
+        'type': fields.selection([('sms', 'SMS'), ('big_ant', 'Big Ant')], string="Message Type"),
     }
     _defaults = {
         'state': 'draft',
+        'type': 'sms',
     }
+
+
+    def process_send_message_in_queue(self, cr, uid, context=None):
+        self.process_sms_queue(cr, 1, context=context)
+        self.process_big_ant_queue(cr, 1, context=context)
+        logging.getLogger('sms.sms').warning("SENDING SMS Message!")
+
+
+    def process_big_ant_queue(self, cr, uid, context=None):
+        params = {
+            'bigantServer': '10.100.100.200',
+            'port': '6660',
+            'sendLoginName': 'xinxi',
+            'passwordType': 0,
+            'sendPassword': 'xinxi',
+            'contentType': 'Text/Html',
+            'sendUserName': '',
+            'msgId': '',
+
+        }
+
+        sms_ids = self.search(cr, uid, [('state', '=', 'draft'), ('type', '=', 'big_ant')], context=context)
+        for sms in self.browse(cr, uid, sms_ids, context):
+            try:
+                params['recvLoginNames'] = sms.to
+                params['subject'] = sms.subject.encode('utf-8')
+                params['content'] = sms.content.encode('utf-8')
+                resp = urllib2.urlopen(self._big_ant_gateway, urlencode(params), timeout=45)
+
+                if resp.code == 200:
+                    sms_server_id = 'Success'
+                    self.write(cr, uid, [sms.id], {
+                        'sms_server_id': sms_server_id,
+                        'state': 'sent',
+                        'sent_date': time.strftime('%Y-%m-%d %H:%M:%S'),
+                    })
+                else:
+                    logging.getLogger('sms.sms').warning("SENDING Big Ant Message failed!")
+                    self.write(cr, uid, [sms.id], {
+                        'state': 'error',
+                        'sent_date': time.strftime('%Y-%m-%d %H:%M:%S'),
+                        'sms_server_id': 'response error ==>' + resp.code,
+                    })
+            except UnicodeEncodeError, e:
+                logging.getLogger('sms.sms').error("sending Big Ant Message failed!")
+                self.write(cr, uid, [sms.id], {
+                    'state': 'error',
+                    'sent_date': time.strftime('%Y-%m-%d %H:%M:%S'),
+                    'sms_server_id': e.reason,
+                })
+            except Exception, e:
+                logging.getLogger('sms.sms').error("sending Big Ant Message failed!")
+                self.write(cr, uid, [sms.id], {
+                    'state': 'error',
+                    'sent_date': time.strftime('%Y-%m-%d %H:%M:%S'),
+                    'sms_server_id': str(e),
+                })
+
 
     def process_sms_queue(self, cr, uid, context=None):
         params = {
@@ -31,7 +93,7 @@ class sms(osv.Model):
             'LoginName': '72620872',
             'Passwd': '047243',
         }
-        sms_ids = self.search(cr, uid, [('state', '=', 'draft')], context=context)
+        sms_ids = self.search(cr, uid, [('state', '=', 'draft'), ('type', '=', 'sms')], context=context)
         for sms in self.browse(cr, uid, sms_ids, context):
             try:
                 params['send_no'] = sms.to
@@ -66,8 +128,6 @@ class sms(osv.Model):
                     'sent_date': time.strftime('%Y-%m-%d %H:%M:%S'),
                     'sms_server_id': str(e),
                 })
-        logging.getLogger('sms.sms').warning("SENDING SMS!")
-
 
     def send_sms_to_group(self, cr, uid, from_rec, content, model, res_id, group_id, context=None):
 
@@ -111,4 +171,49 @@ class sms(osv.Model):
                 sms = self.pool.get('sms.sms')
                 sid = sms.create(cr, uid, {'from': from_rec, 'to': to, 'content': content,
                                            'model': model, 'res_id': res_id},
+                                 context=context)
+
+
+    def send_big_ant_to_users(self, cr, uid, users, from_rec, subject, content, model, res_id, context=None):
+        to = ','.join(
+            [user.login for user in users])
+        if to:
+            sid = self.create(cr, uid,
+                              {'to': to, 'subject': subject, 'content': content, 'model': model, 'res_id': res_id,
+                               'type': 'big_ant'},
+                              context=context)
+
+    def send_big_ant_to_group(self, cr, uid, from_rec, subject, content, model, res_id, group_id, context=None):
+
+        model_pool = self.pool.get('ir.model.data')
+        model_ids = model_pool.search(cr, 1, [('model', '=', 'res.groups'), ('name', '=', group_id)],
+                                      context=context)
+
+        models = model_pool.browse(cr, 1, model_ids, context=context)
+        if models:
+            groups_pool = self.pool.get("res.groups")
+            group = groups_pool.browse(cr, 1, models[0].res_id, context=context)
+            to = ','.join([user.login for user in group.users])
+
+            if to:
+                sms = self.pool.get('sms.sms')
+                sid = sms.create(cr, uid, {'from': from_rec, 'to': to, 'subject': subject, 'content': content,
+                                           'model': model, 'res_id': res_id, 'type': 'big_ant'},
+                                 context=context)
+
+    def send_big_ant_to_config_group(self, cr, uid, config_group_id, from_rec, subject, content, model, res_id,
+                                 context=None):
+        model_pool = self.pool.get('ir.model.data')
+        model_ids = model_pool.search(cr, 1, [('model', '=', 'project.config.sms'), ('name', '=', config_group_id)],
+                                      context=context)
+        models = model_pool.browse(cr, 1, model_ids, context=context)
+        if models:
+            groups_pool = self.pool.get("project.config.sms")
+            group = groups_pool.browse(cr, 1, models[0].res_id, context=context)
+            to = ','.join([user.login for user in group.users])
+
+            if to:
+                sms = self.pool.get('sms.sms')
+                sid = sms.create(cr, uid, {'from': from_rec, 'to': to, 'subject': subject, 'content': content,
+                                           'model': model, 'res_id': res_id, 'type': 'big_ant'},
                                  context=context)
