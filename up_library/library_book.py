0aec632a8c737df94f172686affbd8967a9a9565
cysnake4713 <cysnake4713@gmail.com>
2013-12-23 18:00:49 +0800

    UPDIS-411 图书管理模块实现 with image

--- up_library/library_book.py
+++ up_library/library_book.py
@@ -2,8 +2,9 @@
 import datetime
 from openerp.osv import osv
 from openerp.osv import fields
-from up_tools import tools
+from up_tools import tools as ctools
 from tools import DEFAULT_SERVER_DATE_FORMAT
+import tools
 
 __author__ = 'cysnake4713'
 
@@ -99,27 +100,59 @@ class LibraryBook(osv.osv):
         #            'category': [lambda o, n: o.id if o else 0 != n, lambda o, n: "分项变成: %s --> %s" % (o.full_name if o else "", n)],
     }
 
-
-    def _get_left_quantity(self, cr, uid, ids, field_name, args, context=None):
+    def _image_resize_image_medium(self, cr, uid, ids, name, args, context=None):
         result = dict.fromkeys(ids, False)
         for obj in self.browse(cr, uid, ids, context=context):
-            record_ids = self.pool.get('library.book.record').search(cr, uid, [('book_id', '=', obj.id),
-                                                                               ('is_returned', '=', False)])
-            result[obj.id] = obj.quantity - len(record_ids)
+            return_dict = dict()
+            return_dict['image_medium'] = tools.image_resize_image_medium(obj.image, size=(275, 145))
+            result[obj.id] = return_dict
+
         return result
 
+    def _get_image(self, cr, uid, ids, name, args, context=None):
+        result = dict.fromkeys(ids, False)
+        for obj in self.browse(cr, uid, ids, context=context):
+            result[obj.id] = tools.image_get_resized_images(obj.image)
+        return result
+
+    def _set_image(self, cr, uid, id, name, value, args, context=None):
+        return self.write(cr, uid, [id], {'image': tools.image_resize_image_big(value), 'have_image': True},
+                          context=context)
+
+    def _has_image(self, cr, uid, ids, name, args, context=None):
+        result = {}
+        for obj in self.browse(cr, uid, ids, context=context):
+            result[obj.id] = obj.image != False
+        return result
 
     _columns = {
         #TODO:State set miss
         'code': fields.char(size=64, string='Code', required=True),
         'name': fields.char(size=256, string='Book Name', required=True),
+        'image': fields.binary("Image",
+                               help="This field holds the image used as avatar for this book, limited to 1024x1024px"),
+        'image_medium': fields.function(_get_image, fnct_inv=_set_image,
+                                        string="Medium-sized image", type="binary", multi="_get_image",
+                                        store={
+                                            'library.book.book': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
+                                        },
+                                        help="Medium-sized image of this book. It is automatically " \
+                                             "resized as a 128x128px image, with aspect ratio preserved. " \
+                                             "Use this field in form views or some kanban views."),
+        'image_small': fields.function(_get_image, fnct_inv=_set_image,
+                                       string="Small-sized image", type="binary", multi="_get_image",
+                                       store={
+                                           'library.book.book': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
+                                       },
+                                       help="Small-sized image of this contact. It is automatically " \
+                                            "resized as a 64x64px image, with aspect ratio preserved. " \
+                                            "Use this field anywhere a small image is required."),
+        'has_image': fields.function(_has_image, type="boolean"),
         'category': fields.many2one('library.book.category', required=True, string='Category'),
         'type': fields.many2one('library.book.type', required=True, string='Type'),
         'author': fields.char(size=32, string='Book Author'),
         'publisher': fields.char(size=128, string='Publisher'),
         'price': fields.float(digits=(16, 2), string='Price'),
-        'quantity': fields.integer(string='Quantity'),
-        'quantity_left': fields.function(_get_left_quantity, type='integer', readonly=True, string='Quantity Left'),
         'purchase_date': fields.date(string='Purchase Date'),
         'comment': fields.text(string='Comment'),
         'state': fields.selection(string='State',
@@ -130,7 +163,6 @@ class LibraryBook(osv.osv):
     }
 
     _defaults = {
-        'quantity': 1,
         'state': 'in_store',
         'purchase_date': lambda *a: str(datetime.date.today()),
     }
@@ -153,7 +185,7 @@ class LibraryCategory(osv.osv):
     def _get_full_name(self, cr, uid, ids, field_name, args, context=None):
         result = dict.fromkeys(ids, False)
         for obj in self.browse(cr, uid, ids, context=context):
-            full_name = tools.get_parent_name(obj)
+            full_name = ctools.get_parent_name(obj)
             result[obj.id] = full_name
         return result
 
