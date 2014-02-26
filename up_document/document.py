c77799dec335f498d2ac263cc394b5dff2ec36b2
cysnake4713 <cysnake4713@gmail.com>
2014-02-26 17:39:16 +0800

    UPDIS-426 下载控件完成

--- up_document/document.py
+++ up_document/document.py
@@ -1,9 +1,12 @@
 # -*- encoding: utf-8 -*-
+import base64
+import cStringIO
+from zipfile import ZipFile
+
 __author__ = 'cysnake4713'
 
 from openerp.osv import osv
 from openerp.osv import fields
-from openerp import SUPERUSER_ID
 from openerp.tools.translate import _
 
 
@@ -36,14 +39,15 @@ class DocumentDirectoryAction(osv.osv_memory):
         record_id = context and context.get('active_id', False) or False
         self_record = self.browse(cr, uid, ids[0], context)
         action_obj = self.pool.get("ir.actions.act_window")
-        action_id = action_obj.create(cr, 1, {'name': self_record.name,
-                                              'res_model': 'ir.attachment',
-                                              'view_type': 'form',
-                                              'view_mode': 'tree',
-                                              'domain': [('parent_id', 'child_of', record_id)],
-                                              'context': {
-                                                  'tree_view_ref': 'up_document.view_document_file_public_tree',
-                                                  'group_by': 'parent_id'},
+        action_id = action_obj.create(cr, 1, {
+            'name': self_record.name,
+            'res_model': 'ir.attachment',
+            'view_type': 'form',
+            'view_mode': 'tree',
+            'domain': [('parent_id', 'child_of', record_id)],
+            'context': {
+                'tree_view_ref': 'up_document.view_document_file_public_tree',
+                'group_by': 'parent_id'},
         }, context=context)
 
         return self.pool.get("document.directory").write(cr, uid, [record_id], {'related_action_id': action_id},
@@ -57,7 +61,6 @@ class DocumentDirectoryInherit(osv.osv):
         'related_action_id': fields.many2one('ir.actions.act_window', string='Related Menu Action', ondelete='cascade'),
     }
 
-
     def delete_related_action(self, cr, uid, ids, context):
         directory_records = self.browse(cr, uid, ids, context)
         for directory_record in directory_records:
@@ -66,6 +69,7 @@ class DocumentDirectoryInherit(osv.osv):
             self.pool.get("ir.actions.act_window").unlink(cr, 1, action_id, context)
         return True
 
+    # noinspection PyUnusedLocal
     def onchange_parent_id(self, cr, uid, ids, parent_id, context=None):
         ret = {'value': {}}
         if parent_id:
@@ -82,6 +86,7 @@ class DocumentDirectoryInherit(osv.osv):
             sms_vals = {
                 'group_ids': new_group_ids,
             }
+            # noinspection PyTypeChecker
             ret['value'].update(sms_vals)
         return ret
 
@@ -168,23 +173,8 @@ class IrAttachmentInherit(osv.osv):
             self._check_group_write_privilege(cr, uid, ids, context)
         return super(IrAttachmentInherit, self).write(cr, uid, ids, vals, context)
 
-
+    # noinspection PyUnusedLocal
     def _get_file_size(self, cr, uid, ids, field_name, arg, context):
-        """
-            Either way, it must return a dictionary of values of the form
-            {'id_1_': 'value_1_', 'id_2_': 'value_2_',...}.
-
-            If multi is set, then field_name is replaced by field_names:
-            a list of the field names that should be calculated.
-            Each value in the returned dictionary is also a dictionary from field name to value.
-            For example, if the fields 'name', and 'age' are both based on the vital_statistics function,
-            then the return value of vital_statistics might look like this when ids is [1, 2, 5]:
-            {
-                1: {'name': 'Bob', 'age': 23},
-                2: {'name': 'Sally', 'age', 19},
-                5: {'name': 'Ed', 'age': 62}
-            }
-        """
         result = dict.fromkeys(ids, False)
         for attachment in self.browse(cr, uid, ids, context=context):
             result[attachment.id] = attachment.file_size / (1024.0 * 1024.0)
@@ -198,11 +188,11 @@ class IrAttachmentInherit(osv.osv):
 class IrAttachmentDownloadWizard(osv.osv_memory):
     _name = "ir.attachment.download.wizard"
 
-    def default_get(self, cr, uid, fields, context=None):
+    def default_get(self, cr, uid, field_list, context=None):
         """
         This function gets default values
         """
-        res = super(IrAttachmentDownloadWizard, self).default_get(cr, uid, fields, context=context)
+        res = super(IrAttachmentDownloadWizard, self).default_get(cr, uid, field_list, context=context)
         if context is None:
             context = {}
         record_ids = context and context.get('active_ids', False) or False
@@ -210,10 +200,16 @@ class IrAttachmentDownloadWizard(osv.osv_memory):
             return res
         attachment_obj = self.pool.get('ir.attachment')
         attachments = attachment_obj.browse(cr, uid, record_ids, context=context)
-        if 'attachment_ids' in fields:
+        total_size = reduce(lambda x, y: y.file_size + x, attachments, 0)
+        # total_size = reduce(self.test, attachments)
+        # TODO: need some much useful limit
+        if total_size > 200 * 1024 * 1024:
+            raise osv.except_osv(_('Warning!'), _('Too Large the file will be generate!'))
+        if len(record_ids) > 20:
+            raise osv.except_osv(_('Warning!'), _('Too Many Attachments you choose!'))
+        if 'attachment_ids' in field_list:
             res['attachment_ids'] = [a.id for a in attachments]
         return res
-
 
     _columns = {
         'name': fields.char('File Name', readonly=True),
@@ -225,6 +221,40 @@ class IrAttachmentDownloadWizard(osv.osv_memory):
         'state': fields.selection([('choose', 'choose'),  # choose language
                                    ('get', 'get')])  # get the file
     }
+
+    _defaults = {
+        'state': 'choose',
+    }
+
+    def button_download_files(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        record_ids = context and context.get('active_ids', False) or False
+        attachments = self.pool.get('ir.attachment').browse(cr, uid, record_ids, context)
+        buf = cStringIO.StringIO()
+        with ZipFile(buf, 'w') as zip_obj:
+            for attachment in attachments:
+                if attachment.datas is not False:
+                    file_name = attachment.name
+                    file_data = attachment.datas
+                    zip_obj.writestr(file_name, file_data)
+        # tools.trans_export(lang, mods, buf, this.format, cr)
+        filename = u"附件.zip"
+        out = base64.encodestring(buf.getvalue())
+        buf.close()
+        self.write(cr, uid, ids, {'state': 'get',
+                                  'data': out,
+                                  'name': filename}, context=context)
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'ir.attachment.download.wizard',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'res_id': ids[0],
+            'views': [(False, 'form')],
+            'target': 'new',
+        }
+
 
 # class FileConfig(osv.osv):
 #     _name = 'sfile.file.config'
