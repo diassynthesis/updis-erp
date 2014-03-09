05656c0990bb44bb9481c5d8962207c3ed658219
cysnake4713 <cysnake4713@gmail.com>
2014-03-09 13:39:34 +0800

    UPDIS-443 测试文档下载的性能 脚本更新完毕,待测试

--- up_document/script/move_file_script.py
+++ up_document/script/move_file_script.py
@@ -1,17 +1,51 @@
 #!/usr/bin/python
+
 __author__ = 'cysnake4713', 'Giulio Marcon'
 
+import httplib
 import xmlrpclib
 
-username = 'admin' #the user
-pwd = 'updis_admin_2013'      #the password of the user
-dbname = 'develop'    #the database
+username = 'admin'  #the user
+pwd = 'updis_admin_2013'  #the password of the user
+dbname = 'develop'  #the database
 OPENERP_URL = 'localhost:8069'
+
+
+class TimeoutHTTPConnection(httplib.HTTPConnection):
+    def __init__(self, host, timeout=10):
+        httplib.HTTPConnection.__init__(self, host, timeout=timeout)
+        # self.set_debuglevel(99)
+        #self.sock.settimeout(timeout)
+
+
+"""
+class TimeoutHTTP(httplib.HTTP):
+    _connection_class = TimeoutHTTPConnection
+    def set_timeout(self, timeout):
+        self._conn.timeout = timeout
+"""
+
+
+class TimeoutTransport(xmlrpclib.Transport):
+    def __init__(self, timeout=10, *l, **kw):
+        xmlrpclib.Transport.__init__(self, *l, **kw)
+        self.timeout = timeout
+
+    def make_connection(self, host):
+        conn = TimeoutHTTPConnection(host, self.timeout)
+        return conn
+
+
+class TimeoutServerProxy(xmlrpclib.ServerProxy):
+    def __init__(self, uri, timeout=10, *l, **kw):
+        kw['transport'] = TimeoutTransport(timeout=timeout, use_datetime=kw.get('use_datetime', 0))
+        xmlrpclib.ServerProxy.__init__(self, uri, *l, **kw)
+
 
 # Get the uid
 sock_common = xmlrpclib.ServerProxy('http://' + OPENERP_URL + '/xmlrpc/common')
 uid = sock_common.login(dbname, username, pwd)
-sock = xmlrpclib.ServerProxy('http://' + OPENERP_URL + '/xmlrpc/object')
+sock = TimeoutServerProxy('http://' + OPENERP_URL + '/xmlrpc/object', timeout=1000)
 
 
 def migrate_attachment(att_id):
