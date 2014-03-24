4580bb7526302d131f5cd58a17fbcd9edbf6e4cc
cysnake4713 <cysnake4713@gmail.com>
2014-03-24 15:05:34 +0800

    UPDIS-451 员工生日祝福模块

--- updis/hr.py
+++ updis/hr.py
@@ -141,7 +141,7 @@ class EmployeeBirthdayWish(osv.osv):
             "select id from hr_employee " +
             "Where date_part('day', birthday) = date_part('day', CURRENT_DATE) And date_part('MONTH', birthday) = date_part('MONTH', CURRENT_DATE)")
         employee_ids = cr.fetchall()
-        employees = self.pool.get('hr.employee').read(cr, uid, list(reduce(lambda x, y: x + y, employee_ids)), ['name'])
+        employees = self.pool.get('hr.employee').read(cr, uid, list(reduce(lambda x, y: x + y, employee_ids, [])), ['name'])
         total_wish = len(self.search(cr, 1, []))
         random_wish = int(math.ceil(total_wish * random.random()))
         wishes = self.browse(cr, 1, random_wish).name if random_wish > 0 else ''
