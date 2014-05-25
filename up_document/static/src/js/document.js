0d56eb8215611b314c2cc0e00f734df016987dc5
cysnake4713 <cysnake4713@gmail.com>
2014-05-25 16:04:59 +0800

    Tag 变成 mult select

--- up_document/static/src/js/document.js
+++ up_document/static/src/js/document.js
@@ -143,5 +143,30 @@ openerp.up_document = function (instance, m) {
         }
     });
 
+    instance.web.form.FieldMany2ManyTags.include({
+        _search_create_popup: function (view, ids, context) {
+            var self = this;
+            var pop = new instance.web.form.SelectCreatePopup(this);
+            pop.select_element(
+                self.field.relation,
+                {
+                    title: (view === 'search' ? _t("Search: ") : _t("Create: ")) + this.string,
+                    initial_ids: ids ? _.map(ids, function (x) {
+                        return x[0]
+                    }) : undefined,
+                    initial_view: view,
+                    disable_multiple_selection: false
+                },
+                self.build_domain(),
+                new instance.web.CompoundContext(self.build_context(), context || {})
+            );
+            pop.on("elements_selected", self, function (element_ids) {
+                _.each(element_ids,function (element_id) {
+                    self.add_id(element_id);
+                });
+                self.focus();
+            });
+        }
+    });
 };
 
