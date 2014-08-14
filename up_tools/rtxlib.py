cadb90d12befff5bd1e9be107f58294409fa77a3
cysnake4713 <cysnake4713@gmail.com>
2014-08-14 17:41:42 +0800

    UPDIS-557 rtx接口实现 发送信息接口完成，等待测试 目前是NOTIFY方式

--- up_tools/rtxlib.py
+++ up_tools/rtxlib.py
@@ -6,16 +6,18 @@ from suds.client import Client
 
 class RtxClient(object):
     def __init__(self):
-        self.client = Client(config.get('rtx_address', ''))
         self.rtx_user = config.get('rtx_user', '')
         self.rtx_password = config.get('rtx_password', '')
         self.rtx_key = config.get('rtx_key', '')
         self.session_id = '{F9A239EB-1728-4608-A4D3-7B17BAEB9F18}'
 
+    def client(self):
+        return Client(config.get('rtx_address', ''), timeout=5)
+
 
 if __name__ == '__main__':
     url = 'http://113.108.103.8:8089/RTX_Service.asmx?wsdl'
-    client = Client(url)
+    client = Client(url, timeout=1)
     # a = client.service.AddUser(userName='1111111', userPwd='111', DeptName='',  ChsName='', IGender=1, Cell='', Email='', Phone='',
     # Position='', AuthTYpe=0, key='tianvService2014')
     # a = client.service.IsUserExist(userName='kendy', key='tianvService2014')
