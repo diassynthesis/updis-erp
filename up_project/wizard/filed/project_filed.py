015a21d2e959a545ac885f0c78b87934761358db
cysnake4713 <cysnake4713@gmail.com>
2014-09-28 12:34:10 +0800

    UPDIS-561 项目强制归档按钮 功能 进行更新

--- up_project/wizard/filed/project_filed.py
+++ up_project/wizard/filed/project_filed.py
@@ -21,3 +21,10 @@ class project_project_inherit(osv.osv):
         self.write(cr, uid, ids, {'state': 'project_filed'}, context=context)
         self._workflow_signal(cr, uid, ids, 's_filed_start', context=context),
         return ids[0]
+
+    def force_project_filed(self, cr, uid, ids, context=None):
+        self._workflow_signal(cr, uid, ids, 'process_finish', context=context),
+        self._workflow_signal(cr, uid, ids, 's_filed_start', context=context),
+        self._workflow_signal(cr, uid, ids, 's_filed_filing_finish', context=context),
+        # self.write(cr, uid, ids, {'state': 'project_finish'}, context=context)
+        return True
