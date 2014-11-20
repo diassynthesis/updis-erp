b6bc98a61185f1c5dc2dd0cf87b2a4951fad9c7c
cysnake4713 <cysnake4713@gmail.com>
2014-11-20 14:38:50 +0800

    UPDIS-640 签名图片显示问题

--- signature/static/src/js/signature.js
+++ signature/static/src/js/signature.js
@@ -15,31 +15,33 @@ openerp.signature = function (instance) {
             var self = this;
             var parent_render = this._super;
             self.effective_readonly = true;
-            user = this.get('value');
-            new instance.web.Model('res.users').call('get_user_image', [user[0]]).done(function (result) {
-                //如果有签名图片，则显示签名图片，
-                if (result != null) {
-                    var url = 'data:image/png;base64,' + result;
-                    var $img = $(QWeb.render("FieldBinaryImage-img", { widget: self, url: url }));
-                    self.$el.find('> img').remove();
-                    self.$el.prepend($img);
-                    $img.load(function () {
-                        if (!self.options.size)
-                            return;
-                        $img.css("max-width", "" + self.options.size[0] + "px");
-                        $img.css("max-height", "" + self.options.size[1] + "px");
-                        $img.css("margin-left", "" + (self.options.size[0] - $img.width()) / 2 + "px");
-                        $img.css("margin-top", "" + (self.options.size[1] - $img.height()) / 2 + "px");
-                    });
-                    $img.on('error', function () {
-                        $img.attr('src', self.placeholder);
-                        instance.webclient.notification.warn(_t("Image"), _t("Could not display the selected image."));
-                    });
-                    //如果没有签名图片，则显示用户名称。
-                } else {
-                    self.$el.prepend(user[1]);
-                }
-            });
+            var user = this.get('value');
+            if (user){
+                new instance.web.Model('res.users').call('get_user_image', [user[0]]).done(function (result) {
+                    //如果有签名图片，则显示签名图片，
+                    if (result != null) {
+                        var url = 'data:image/png;base64,' + result;
+                        var $img = $(QWeb.render("FieldBinaryImage-img", { widget: self, url: url }));
+                        self.$el.find('> img').remove();
+                        self.$el.prepend($img);
+                        $img.load(function () {
+                            if (!self.options.size)
+                                return;
+                            $img.css("max-width", "" + self.options.size[0] + "px");
+                            $img.css("max-height", "" + self.options.size[1] + "px");
+                            $img.css("margin-left", "" + (self.options.size[0] - $img.width()) / 2 + "px");
+                            $img.css("margin-top", "" + (self.options.size[1] - $img.height()) / 2 + "px");
+                        });
+                        $img.on('error', function () {
+                            $img.attr('src', self.placeholder);
+                            instance.webclient.notification.warn(_t("Image"), _t("Could not display the selected image."));
+                        });
+                        //如果没有签名图片，则显示用户名称。
+                    } else {
+                        self.$el.prepend(user[1]);
+                    }
+                });
+            }
         },
     });
 
