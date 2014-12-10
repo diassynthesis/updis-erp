b7ac4794cf4d1146c846912f0db34ce1bd730bbb
cysnake4713 <cysnake4713@gmail.com>
2014-12-10 17:51:48 +0800

    UPDIS-663 项目2011HJ12增加项目负责人黄文钦，同时限定项目负责人为空时不能归档

--- up_project/wizard/process/project_process.py
+++ up_project/wizard/process/project_process.py
@@ -1,5 +1,6 @@
 # -*- encoding:utf-8 -*-
-from osv import osv, fields
+from openerp import exceptions
+from openerp.osv import osv, fields
 
 
 class project_project_inherit(osv.osv):
@@ -35,5 +36,11 @@ class project_project_inherit(osv.osv):
         return True
 
     def button_process_finish(self, cr, uid, ids, context=None):
+        # 如果没有项目负责人，或者项目负责人都离院，则不能归档
+        project_users = self.read(cr, uid, ids[0], ['user_id'], context)['user_id']
+        project_users = self.pool['res.users'].search(cr, uid, [('id', 'in', project_users), ("active", "=", True)], context=context)
+        if not len(project_users):
+            raise exceptions.Warning(u'本项目没有项目负责人,或者当前项目负责人已经离院。请在项目成员标签卡下，填写项目负责人或主管总师变更变更申请后再进行归档！')
         self._workflow_signal(cr, uid, ids, 'process_finish', context=context)
+        self.write(cr, uid, ids, {'state': 'project_filed'}, context=context)
         return self.button_filed_filing_form(cr, uid, ids, context)
