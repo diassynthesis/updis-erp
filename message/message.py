d0ea623a059f659cdd23df44d9f436cb70358a54
Med Said BARA <diassynthesis@gmail.com>
2019-03-29 18:35:06 +0100

    Update message.py
    Adding column: mobile_phone

--- message/message.py
+++ message/message.py
@@ -34,6 +34,7 @@ class MessageCategory(osv.Model):
     _order = "name"
     _columns = {
         'name': fields.char("Title", size=128),
+        'mobile_phone': fields.Char('phone Number'),
         'default_message_count': fields.integer('Default message count',
                                                 help="How many messages should be displayed in front door for this category?"),
         'message_meta': fields.char('Meta',
